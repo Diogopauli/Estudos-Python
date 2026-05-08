48d3b63c671e98bab8bfed7cc7b9dd9301628ae0
Diogopauli <diogooliveirapaulino@gmail.com>
2026-05-08 17:07:51 -0300

    Importando arquivo estudos python 1

---
+++ Basico.py
@@ -0,0 +1,25 @@
+#Operações matemáticas
+
+testeMultiplicacao1 = 13 * 5
+testeMultiplicacao2 = "Im blue\n" + 3 * "daa bee dee daa bee daa\n"
+testeMultiplicacao3 = [1,2,3,4] * 3
+print(testeMultiplicacao2)
+testeSoma1= 1+2+6,
+testeSoma2 = "Eu " + "amo a piton"
+testeSoma3 = [1,2,3,5,6] + [2,5,12,7,1]
+testeSomaPares = [0,2,4,6,8]
+testeSomaImpares = [1,3,5,7,9]
+testeSomaImparMaisPar = testeSomaPares + testeSomaImpares
+testeSoma4 = testeSomaImparMaisPar
+print(testeSoma2)
+print(testeSoma3)
+print(testeSoma4)
+
+#Lista
+testeLista1=[]
+testeLista2=[1,2,3,4,5,6,7,8,9]
+for x in testeLista2:
+    print(x)
+
+#Simples linha de print
+print("This line will be printed.")
