034eaf2c7e6209781c576099eb8289bb75934891
Diogopauli <diogooliveirapaulino@gmail.com>
2026-06-01 17:50:54 -0300

    Adicionando introdução com hello word

---
+++ Introducao.py
@@ -0,0 +1,5 @@
+print("Hello Word!")
+# O Python é uma linguagem que se assemelha muito ao inglês utilizado popularmente,
+# Com ele, podemos fazer operações poderosas na programação com poucas linhas de código
+# o print, utiliza-se parêntese para definir o que será exibido, no caso, não utilizei nenhuma
+# variável, mas sim uma string criada dentro do próprio print, definida por aspas ""
